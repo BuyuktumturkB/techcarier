a13d01817a7f99ef717ce90894ad8c2dad62b9c7
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2025-03-14 10:46:58 +0300

    Create TestData.js

---
+++ src/Database/TestData.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const TestData=[
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    },
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    },
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    },
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    },
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    },
+    {
+        title:'C# Teknik Test (Başlangıç Seviye)',
+        image:'https://www.techcareer.net/_next/image?url=https%3A%2F%2Fcdn.gcp.techcareer.net%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2%2FTestler_kapak_goersel_boyutu_7_2d0f5d4fc2.png&w=384&q=75',
+        description:'Bu C# kodlama testi adayların C# dilinde temel seviye algoritmalar ile programlama',
+        level:'Başlangıç',
+        time:16,
+        quantity:10
+    }
+];
+export default TestData
