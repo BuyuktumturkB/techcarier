10071d0241a2e21740e98fd69f0984a833a9065a
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2025-03-14 12:55:58 +0300

    Create ReferencesCard.js

---
+++ src/Components/ReferencesCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ReferencesCard = ()=>{
+
+    return(
+        <div>
+            <div style={{flexDirection:'row', display:'flex', backgroundColor:'white'}}>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F3.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F17.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F2.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F22.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F4.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F8.png&w=256&q=100"
+                alt=""/>
+
+            </div>
+            <div style={{flexDirection:'row', display:'flex', backgroundColor:'white'}}>
+                <img src='https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F5.png&w=256&q=100'
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F35.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F15.png&w=256&q=100"
+                  alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F34.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F34.png&w=256&q=100"
+                alt=""/>
+                <img src="https://www.techcareer.net/_next/image?url=%2Fassets%2Fimages%2Fhomepage-references%2F21.png&w=256&q=100"
+                alt=""/>
+
+            </div>
+        </div>
+    )
+
+};
+export default ReferencesCard;
