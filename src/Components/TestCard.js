6bc6e4059a53117283dbebdf103a9b6d934ddfd6
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2025-03-14 10:44:31 +0300

    Update TestCard.js

--- src/Components/TestCard.js
+++ src/Components/TestCard.js
@@ -1,13 +1,21 @@
-import def from "ajv/dist/vocabularies/discriminator";
+
 import React from "react";
 
-const TestCard=({title,description,image})=>
+const TestCard=({title,description,image,level,time,quantity})=>
     {
         return(
-            <div>
-                <img src={image}/>
+            <div style={{borderWidth:'10px', borderColor:'black'}}>
+                <img style={{width:'70%'}} src={image} alt=" "/>
                 <h2> {title}</h2>
+                <p>{description}</p>
+                <div style={{flexDirection:'row', display:'flex', justifyContent:'space-between'}}>
+                    <p>{level}</p>
+                    <p>{time} dakika</p>
+                    <p>{quantity} soru </p>
+
+                </div>
             </div>
+
         )
     }
     export default TestCard;
