8dca1d3b8f5a8d0d1a1095e83aacca49f2e9c60d
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2025-03-14 11:16:50 +0300

    Update EventsCard.js

--- src/Components/EventsCard.js
+++ src/Components/EventsCard.js
@@ -4,14 +4,15 @@ import React from "react";
 const EventsCard=({title,deathTime,description,imageUrl})=>{
    
    return(
-    <div style={{borderRadius:0,borderWidth:'1px', width:'30%',borderStyle:'solid',
+    <div style={{borderRadius:0,borderWidth:'1px', width:'30%',borderStyle:'solid',margin:'10px',
      borderColor:'gray', justifyContent:'center', alignItems:'center'}}>
         <img src={imageUrl} style={{width:'99%',justifyContent:'center',alignItems:'center'}} />
         <h2>{title}</h2>
         <p>{description}</p>
         <div style={{flexDirection:'row',display:'flex', justifyContent:'space-around'}}>
         <p> Son Tarih: {deathTime}</p>
-        <Button style={{color:'" #47c26d"'}}> Hemen Başvur </Button>
+        <Button style={{backgroundColor:" #47c26d", width:100, height:45}}> 
+         <p style={{color:'white'}}> Başvur</p> </Button>
         </div>
         
     </div>
