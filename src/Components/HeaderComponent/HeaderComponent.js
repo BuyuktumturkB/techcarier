e29ca16b252cb1b539605e4406dcb75c40a2d6cc
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2025-03-14 10:01:03 +0300

    Update HeaderComponent.js

--- src/Components/HeaderComponent/HeaderComponent.js
+++ src/Components/HeaderComponent/HeaderComponent.js
@@ -2,22 +2,24 @@ import React from "react";
 import './HeaderComponent.css'
 import { Button } from "@mui/material";
 
-const HeaderComponent = ({title, description}) =>
-    {
-        return (
-            <div style={{flexDirection:'row', display:'flex', justifyContent:'center'}}>
-                <div style={{flexDirection:'column'}}>
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                    <Button style={{backgroundColor:'#47c26d'}}>Keşfet</Button>
-                    
-
-                </div>
-                <img style={{height:400, width:400}} alt="computer"
-                src="https://www.techcareer.net/assets/images/call-to-actions/pages/quizzes/vector.svg"/>
-
+const HeaderComponent = ({ title, description }) => {
+    return (
+        <div className="header-container">
+            <div className="text-container">
+                <h1>{title}</h1>
+                <p style={{width:'50%',justifyContent:'center'}}>{description}</p>
+                <p style={{borderRadius:10, borderWidth:2, borderColor:'gray'}}>Ücretsizdir</p>
+                <Button style={{backgroundColor:'#47c26d',height:50}}>
+                <p style={{color:'white'}}> Keşfet</p> </Button>
+                
             </div>
-        )
+            <img 
+                className="image" 
+                alt="computer"
+                src="https://www.techcareer.net/assets/images/call-to-actions/pages/quizzes/vector.svg"
+            />
+        </div>
+    );
+};
 
-    }
 export default HeaderComponent;
